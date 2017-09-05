912791059eeb99577e39578dc5ef1b4d30be0dea
Márcio Dias <marciojr91@gmail.com>
2017-09-05 09:48:26 -0300

    Correção no TrackerRouteContainer

--- src/Model/TrackerRouteContainer.php
+++ src/Model/TrackerRouteContainer.php
@@ -11,29 +11,31 @@ class TrackerRouteContainer
 {
 
   private static $routes = [
-    [
-      'pattern' => '/tracker/mouse',
-      'callback' => function($request) {
-          TrackerController::mouseMoveTracker($request);
-      }
-    ],
-    [
-      'pattern' => '/tracker/getPages',
-      'callback' => function($request) {
-          TrackerController::getPages($request);
-      }
-    ],
-    [
-      'pattern' => '/tracker/getPageVersions',
-      'callback' => function($request) {
-          TrackerController::getPageVersions($request);
-      }
-    ],
+
   ];
 
   public static function getRoutes()
   {
-    return self::$routes;
+    return [
+        [
+          'pattern' => '/tracker/mouse',
+          'callback' => function($request) {
+              TrackerController::mouseMoveTracker($request);
+          }
+        ],
+        [
+          'pattern' => '/tracker/getPages',
+          'callback' => function($request) {
+              TrackerController::getPages($request);
+          }
+        ],
+        [
+          'pattern' => '/tracker/getPageVersions',
+          'callback' => function($request) {
+              TrackerController::getPageVersions($request);
+          }
+        ]
+    ];
   }
 
 }
