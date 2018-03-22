7ce31a45d77823794d11509ee343ec3c54ac16a5
xIgor <igorfelipefs@gmail.com>
2018-03-22 15:34:33 -0300

    Salvar os parametros dos metodos GET e POST; Salvar uma copia de qualquer arquivo que foi submetido no sistema por upload

--- src/Middleware/TrackerMiddleware.php
+++ src/Middleware/TrackerMiddleware.php
@@ -3,7 +3,6 @@
 
 namespace Intec\Tracker\Middleware;
 
-
 use Intec\Tracker\Model\DummyTracker;
 use Intec\Tracker\Model\UriTracker;
 use Intec\Router\Request;
@@ -17,26 +16,31 @@ class TrackerMiddleware
   /**
   * Used in routes to get information
   **/
-  public function __construct()
-  {
-
-  }
-
-  public static function dummyTracker(Request $request)
-  {
-    $dTracker = new DummyTracker();
-    $dTracker->log('Hello from TrackerMiddleware!');
-  }
-
-  public static function userTracker(Request $request)
-  {
-
-    $clientTracker = new ClientTracker();
-    $id = $clientTracker->save();
-    if($id) {
-      $adwordsTracker = new GoogleAdwordsTracker($id, $request->getQueryParams());
-      $adwordsTracker->save();
+    public function __construct()
+    {
     }
 
-  }
+    public static function dummyTracker(Request $request)
+    {
+        $dTracker = new DummyTracker();
+        $dTracker->log('Hello from TrackerMiddleware!');
+    }
+
+    public static function userTracker(Request $request)
+    {
+        $clientTracker = new ClientTracker();
+        // Obtendo os parametros do metodo de requisicao
+        $requestMethod = $clientTracker->getRequestMethod();
+        if ($requestMethod == 'GET') {
+            $clientTracker->setRequestParams($request->getQueryParams());
+        } else { // POST
+            $clientTracker->setRequestParams($request->getPostParams());
+        }
+        $clientTracker->setFiles($request->getFilesParams());
+        $id = $clientTracker->save();
+        if($id) {
+          $adwordsTracker = new GoogleAdwordsTracker($id, $request->getQueryParams());
+          $adwordsTracker->save();
+        }
+    }
 }
